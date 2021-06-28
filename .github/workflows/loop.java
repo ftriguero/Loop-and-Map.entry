6d262c7bf29050e84a7eb4cac4c30898d4d493e1
ftriguero <84296453+ftriguero@users.noreply.github.com>
2021-06-27 22:51:30 -0700

    Create loop.java

---
+++ .github/workflows/loop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArrayExample {
+	/** Main method */
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		ArrayList<Double> list = new ArrayList<Double>();
+		
+		System.out.print("Enter 5 numbers: ");
+		for (int i = 0; i < 5; i++) {
+			list.add(input.nextDouble());
+		}
+		System.out.println("Sum of list: " + sum(list));
+	}
+
+	public static double sum(ArrayList<Double> list) {
+		double sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		return sum;
+	}
+}
